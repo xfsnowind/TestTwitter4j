5c1e01e834d881272c604c8468d8ee6d50c5aa8e
fx <feng@feng-Precision-T1600.(none)>
2013-01-25 14:22:11 +0100

    add pom.xml and web.xml, create the file structure

---
+++ src/main/java/com/nesstar/TestTwitter4jServlet.java
@@ -0,0 +1,12 @@
+package com.nesstar;
+
+import javax.servlet.http.HttpServlet;
+
+public class TestTwitter4jServlet extends HttpServlet{
+
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 1L;
+
+}
