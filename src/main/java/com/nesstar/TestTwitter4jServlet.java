9f22a23de202c8fc8437ee7b2fd802d2960f8c06
fx <feng@feng-Precision-T1600.(none)>
2013-01-25 14:41:42 +0100

    simply make the projet run through type: mvn jetty:run, and then check the localhost:8080/TestTwitter4j/t4j

--- src/main/java/com/nesstar/TestTwitter4jServlet.java
+++ src/main/java/com/nesstar/TestTwitter4jServlet.java
@@ -1,6 +1,13 @@
 package com.nesstar;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 
 public class TestTwitter4jServlet extends HttpServlet{
 
@@ -8,5 +15,25 @@ public class TestTwitter4jServlet extends HttpServlet{
     * 
     */
    private static final long serialVersionUID = 1L;
+   
+   private static final Logger logger = Logger.getLogger(TestTwitter4jServlet.class);
+
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      logger.info("test got");
+      super.doGet(req, resp);
+   }
+
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      super.doPost(req, resp);
+   }
+
+   @Override
+   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      logger.info("test service");
+      super.service(req, resp);
+   }
 
 }
