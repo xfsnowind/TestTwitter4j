27561c81c25ec0352e58a623137bfd54eda8f68c
fx <feng@feng-Precision-T1600.(none)>
2013-01-27 17:40:42 +0100

    use old consumer key and access token, just use to test twitter api

---
+++ src/main/java/com/nesstar/StaticData.java
@@ -0,0 +1,21 @@
+package com.nesstar;
+
+public class StaticData {
+   
+   public static final String CONSUMER_SECRET = "WgoGJnLpjHb1V3tbprPVV1ztXmAC0smgnuiMH31lD8";
+   
+   public static final String CONSUMER_KEY = "Fn6jljSlMiI67pzUIbfj9g";
+   
+   public static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize";
+   
+   public static final String CALLBACK_URL = "http://code.google.com/p/ikt411sosapplicationandroid/";
+   
+   public static final String ACCESS_TOKEN = "35186489-GeR21jK1P5OxH1Gy2Dh16dBeRInrfAatoAYAjVG0I";
+   
+   public static final String ACCESS_TOKEN_SECRET = "GBMSbo5xgls4KJM2eLbTuJv9b3Fkpr5Pt0rNcIQgog";
+   
+   public static final String ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token";
+   
+   public static final String REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token";
+
+}
--- src/main/java/com/nesstar/TestTwitter4jServlet.java
+++ src/main/java/com/nesstar/TestTwitter4jServlet.java
@@ -2,18 +2,19 @@ package com.nesstar;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 public class TestTwitter4jServlet extends HttpServlet{
 
@@ -24,43 +25,19 @@ public class TestTwitter4jServlet extends HttpServlet{
    
    private static final Logger logger = Logger.getLogger(TestTwitter4jServlet.class);
    
-   private static final String CALLBACK_URL = "http://testtwitter4j.com";
-   
-   private static final String RESOURCE_URL = "https://api.twitter.com/oauth/request_token";
-
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      HttpClient client = new DefaultHttpClient(); 
+      Twitter twitter = TwitterFactory.getSingleton();
+      List<Status> statuses = null;
       try {
-         HttpGet httpGet = new HttpGet(RESOURCE_URL + "?oauth_callback=" + CALLBACK_URL);
-         
-         HttpResponse response = client.execute(httpGet);
-         
-         HttpEntity entity = response.getEntity();
-         
-         if (null != entity) {
-            InputStream instream = entity.getContent();
-            logger.info(entity.toString());
-            try {
-               instream.read();
-               // do something useful with the response
-           } catch (IOException ex) {
-               // In case of an IOException the connection will be released
-               // back to the connection manager automatically
-               throw ex;
-           } catch (RuntimeException ex) {
-               // In case of an unexpected exception you may want to abort
-               // the HTTP request in order to shut down the underlying
-               // connection immediately.
-               httpGet.abort();
-               throw ex;
-           } finally {
-               // Closing the input stream will trigger connection release
-               try { instream.close(); } catch (Exception ignore) {}
-           }
-         }
-      } finally {
-         client.getConnectionManager().shutdown();
+         statuses = twitter.getHomeTimeline();
+      } catch (TwitterException e) {
+         logger.error(e.getMessage());
+      }
+      logger.info("Showing home timeline.");
+      for (Status status : statuses) {
+          logger.info(status.getUser().getName() + ":" +
+                             status.getText());
       }
    }
 
